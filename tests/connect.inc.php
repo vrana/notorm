ff0e49de8616b1c4c53c8780258d6c53b64662ed
Jakub Vrana <jakub@vrana.cz>
2010-04-30 18:03:05 +0200

    Run from any directory

--- tests/connect.inc.php
+++ tests/connect.inc.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
-include "../NotORM.php";
+include dirname(__FILE__) . "/../NotORM.php";
 
 $connection = new PDO("mysql:host=127.0.0.1;dbname=software", "ODBC");
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
--- tests/run-tests.php
+++ tests/run-tests.php
@@ -1,8 +1,8 @@
 <?php
 $start = microtime(true);
-foreach (glob("*.phpt") as $filename) {
+foreach (glob(dirname(__FILE__) . "/*.phpt") as $filename) {
 	ob_start();
-	include dirname(__FILE__) . "/$filename";
+	include $filename;
 	if (!preg_match("~^--TEST--\n(.*)\n--FILE--\n(.*)\n--EXPECTF--\n(.*)~s", ob_get_clean(), $match)) {
 		echo "wrong test in $filename\n";
 	} elseif (rtrim($match[2]) != rtrim($match[3])) {
