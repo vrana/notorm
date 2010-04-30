9383f8d1065e04c0e5659d0bed10fc760cbeba75
Jakub Vrana <jakub@vrana.cz>
2010-04-30 17:51:34 +0200

    Dibi discovery

--- NotORM/Structure.php
+++ NotORM/Structure.php
@@ -87,19 +87,33 @@ class NotORM_Structure_Discovery implements NotORM_Structure {
 	private $connection;
 	
 	/** Create autodisovery structure
-	* @param PDO
-	* @param string
+	* @param PDO|DibiConnection
 	*/
-	function __construct(PDO $connection) {
+	function __construct($connection) {
+		$exception = "Argument 1 passed to NotORM_Structure_Discovery::__construct() must be an instance of PDO or DibiConnection";
+		if (!is_object($connection)) {
+			throw new InvalidArgumentException("$exception, " . gettype($connection) . " given");
+		}
+		if (!($connection instanceof PDO || $connection instanceof DibiConnection)) {
+			throw new InvalidArgumentException("$exception, instance of " . get_class($connection) . " given");
+		}
 		$this->connection = $connection;
 	}
 	
 	function getPrimary($table) {
-		$return = null;
+		if ($this->connection instanceof DibiConnection) {
+			foreach ($this->connection->getDriver()->getIndexes($table) as $index) {
+				if ($index['primary'] && count($index['columns']) == 1) {
+					return $index['columns'][0];
+				}
+			}
+			return false;
+		}
+		$return = false;
 		foreach ($this->connection->query("EXPLAIN $table") as $column) {
 			if ($column["Key"] == "PRI") {
 				if (isset($return)) {
-					return null; // multi-column primary key is not supported
+					return false; // multi-column primary key is not supported
 				}
 				$return = $column["Field"];
 			}
@@ -108,6 +122,18 @@ class NotORM_Structure_Discovery implements NotORM_Structure {
 	}
 	
 	function getReferencingColumn($name, $table) {
+		if ($this->connection instanceof DibiConnection) {
+			$primary = $this->getPrimary($table);
+			foreach ($this->connection->getDriver()->getForeignKeys($name) as $foreign) {
+				if ($foreign["table"] == $table) {
+					$key = array_search($primary, $foreign["foreign"]);
+					if ($key !== false) {
+						return $foreign["local"][$key];
+					}
+				}
+			}
+			return false;
+		}
 		return $this->connection->query("
 			SELECT COLUMN_NAME
 			FROM information_schema.KEY_COLUMN_USAGE
@@ -128,6 +154,15 @@ class NotORM_Structure_Discovery implements NotORM_Structure {
 	}
 	
 	function getReferencedTable($name, $table) {
+		if ($this->connection instanceof DibiConnection) {
+			foreach ($this->connection->getDriver()->getForeignKeys($table) as $foreign) {
+				$key = array_search($name, $foreign["local"]);
+				if ($key !== false) {
+					return $foreign["table"];
+				}
+			}
+			return false;
+		}
 		return $this->connection->query("
 			SELECT REFERENCED_TABLE_NAME
 			FROM information_schema.KEY_COLUMN_USAGE
--- tests/connect.inc.php
+++ tests/connect.inc.php
@@ -3,9 +3,11 @@ error_reporting(E_ALL | E_STRICT);
 include "../NotORM.php";
 
 $connection = new PDO("mysql:host=127.0.0.1;dbname=software", "ODBC");
+//~ $connection = new PDO("sqlite:software.sdb");
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 /*
-include 'dibi.php';
+include "dibi.php";
 $connection = new DibiConnection(array('database' => 'software'));
+//~ $connection = new DibiConnection(array('driver' => 'sqlite3', 'database' => 'software.sdb'));
 */
 $software = new NotORM($connection); //~ , new NotORM_Structure_Discovery($connection)
