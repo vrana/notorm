034db27a20a50146fda311c2e4335b70bcaac039
Jakub Vrana <jakub@vrana.cz>
2010-04-30 18:03:05 +0200

    Run from any directory

--- tests/connect.inc.php
+++ tests/connect.inc.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(E_ALL | E_STRICT);
-include "../NotORM.php";
+include dirname(__FILE__) . "/../NotORM.php";
 
 $connection = new PDO("mysql:host=127.0.0.1;dbname=software", "ODBC");
 //~ $connection = new PDO("sqlite:software.sdb");
