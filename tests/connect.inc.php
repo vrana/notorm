ff5c5966a525e849029647ab79fb707a466feaf2
Jakub Vrana <jakub@vrana.cz>
2010-05-02 07:57:33 +0200

    Discovery test

--- tests/connect.inc.php
+++ tests/connect.inc.php
@@ -2,12 +2,7 @@
 error_reporting(E_ALL | E_STRICT);
 include dirname(__FILE__) . "/../NotORM.php";
 
-$connection = new PDO("mysql:host=127.0.0.1;dbname=software", "ODBC");
-//~ $connection = new PDO("sqlite:software.sdb");
-$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
-/*
 include "dibi.php";
 $connection = new DibiConnection(array('database' => 'software'));
 //~ $connection = new DibiConnection(array('driver' => 'sqlite3', 'database' => 'software.sdb'));
-*/
-$software = new NotORM($connection); //~ , new NotORM_Structure_Discovery($connection)
+
