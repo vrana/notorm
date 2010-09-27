0bee6abe8219346f743cdbe366d0c553f2e48b9e
Jakub Vrana <jakub@vrana.cz>
2010-09-27 18:23:46 +0200

    Use default database host

--- tests/connect.inc.php
+++ tests/connect.inc.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL | E_STRICT);
 include dirname(__FILE__) . "/../NotORM.php";
 
-$connection = new PDO("mysql:host=127.0.0.1;dbname=software", "ODBC");
+$connection = new PDO("mysql:dbname=software", "ODBC");
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 $connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
 $software = new NotORM($connection);
