d1f17d286200b61ac2687ec8d03aa3c045ff8353
David Grudl <david@grudl.com>
2010-12-08 14:35:43 +0100

    fixed tests

--- tests/connect.inc.php
+++ tests/connect.inc.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL | E_STRICT);
 include dirname(__FILE__) . "/../NotORM/NotORM.php";
 
-$connection = new PDO("mysql:dbname=software", "ODBC");
+$connection = new PDO("mysql:dbname=test", "test");
 $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 $connection->setAttribute(PDO::ATTR_CASE, PDO::CASE_LOWER);
 $software = new NotORM($connection);
