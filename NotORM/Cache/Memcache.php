0843116ec567ed621ad698227616ae20fa56fc51
Felipe Girotti <felipe.girotti@gmail.com>
2012-11-24 14:36:21 -0200

    This refactory cache for memcache and test pass all

--- NotORM/Cache/Memcache.php
+++ NotORM/Cache/Memcache.php
@@ -1,20 +1,61 @@
 <?php
-
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
- * Description of Memcache
- *
- * @author FELIPE
+ * Based Zend_Cache_Memcached
  */
 namespace NotORM\Cache;
+use \Memcache as MCache;
+class Memcache extends CacheAbstract implements CacheInterface
+{          
+    /**
+     *
+     * @var \Memcache
+     */
+    protected $memcache;    
+   
 
-class Memcache extends CacheAbstract
-{
-    //put your code here
+    
+    public function __construct($options = null) 
+    {
+        if (!extension_loaded('memcache')) {
+           throw new \RuntimeException('The memcache extension must be loaded for using this backend !'); 
+        }
+        $this->memcache = new MCache;        
+    }
+    
+    public function addServer(array $options)
+    {           
+        return call_user_func(array($this->memcache, 'addServer'), implode(',',$options));        
+    }
+        
+    
+    public function clear($key = null)
+    {
+        if (empty($key)) {
+           return $this->memcache->flush();
+        }
+        return $this->memcache->delete($key, 0);
+    }
+    
+    public function load($key)
+    {
+       $load = $this->memcache->get($key);
+       if (is_array($load) && isset($load[0])) {
+           return $load[0];
+       }
+       return false;
+    }
+    
+    public function save($key, $data, $flag = 0)
+    {                       
+        return $this->memcache->set($key, array($data, time(), $this->lifetime), $flag, $this->lifetime);                
+    }
+    
+    public function getMemcache()
+    {
+        return $this->memcache;
+    }
+    
+   
 }
 
 
---
+++ testsPHPUnit/NotORM/Cache/MemcacheTest.php
@@ -0,0 +1,60 @@
+<?php
+namespace NotORM\Cache;
+use NotORM\Cache\Memcache;
+class MemcacheTest extends \PHPUnit_Framework_TestCase
+{
+    protected $memcache;
+    protected $data = array(
+        'host' => '10.1.1.4',        
+        );
+    public function setUp() 
+    {
+        $this->memcache =  new Memcache;        
+    }
+    
+    public function testInstancieMemcache()
+    {
+       $this->assertInstanceOf('NotOrm\Cache\Memcache', $this->memcache);
+    }
+    
+    public function testAddServer()
+    {
+        $returnServer = $this->memcache->addServer($this->data);        
+        $this->assertTrue($returnServer);
+    }
+    /**
+     * @depends testAddServer
+     */
+    public function testSetData()
+    {
+        $this->memcache->addServer($this->data);
+        $save = $this->memcache->save('teste', $this->data);
+        $this->assertTrue($save);
+    }
+    /**
+     * @depends testSetData
+     */
+    public function testGetData()
+    {
+        $this->memcache->addServer($this->data);
+        $this->assertEquals($this->data, $this->memcache->load('teste'));
+    }
+    /**
+     * @depends testGetData
+     */
+    public function testClearAll()
+    {
+        $this->memcache->addServer($this->data);
+        $this->memcache->clear();
+        $this->assertEmpty($this->memcache->load('teste'));
+    }
+    
+    public function testClearDataForKey()
+    {
+        $this->memcache->addServer($this->data);
+        $this->memcache->save('teste2', $this->data);
+        $this->assertEquals($this->data, $this->memcache->load('teste2'));
+        $this->memcache->clear('teste2');
+        $this->assertEmpty($this->memcache->load('teste2'));
+    }
+}
