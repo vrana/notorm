dc5a08772996fdbe36d21828161c7c80c95b5b8d
Felipe Girotti <felipe.girotti@gmail.com>
2012-12-01 22:18:19 -0200

    Formatação de classe

--- NotORM/NotORM.php
+++ NotORM/NotORM.php
@@ -21,56 +21,74 @@ use \PDO,
     
 class NotORM extends NotORMAbstract {
 	
-	/** Create database representation
-	* @param \PDO
-	* @param NotORM\Structure\StructureInterface or null for new NotORM_Structure_Convention
-	* @param NotORM\Cache\CacheInterface or null for no cache
-	*/
-	function __construct(PDO $connection, StructureInterface $structure = null, CacheInterface $cache = null) {
-		$this->connection = $connection;
-		$this->driver = $connection->getAttribute(\PDO::ATTR_DRIVER_NAME);
-		if (!isset($structure)) {
-			$structure = new Convention;
-		}
-		$this->structure = $structure;
-		$this->cache = $cache;
-	}
-	
-	/** Get table data to use as $db->table[1]
-	* @param string
-	* @return NotORM_Result
-	*/
-	function __get($table) {
-		return new Result($this->structure->getReferencingTable($table, ''), $this, true);
-	}
-	
-	/** Set write-only properties
-	* @return null
-	*/
-	function __set($name, $value) {
-		if ($name == "debug" || $name == "freeze" || $name == "rowClass") {
-			$this->$name = $value;
-		}
-		if ($name == "transaction") {
-			switch (strtoupper($value)) {
-				case "BEGIN": return $this->connection->beginTransaction();
-				case "COMMIT": return $this->connection->commit();
-				case "ROLLBACK": return $this->connection->rollback();
-			}
-		}
-	}
-	
-	/** Get table data
-	* @param string
-	* @param array (["condition"[, array("value")]]) passed to NotORM_Result::where()
-	* @return NotORM_Result
-	*/
-	function __call($table, array $where) {
-		$return = new Result($this->structure->getReferencingTable($table, ''), $this);
-		if ($where) {
-			call_user_func_array(array($return, 'where'), $where);
-		}
-		return $return;
-	}
+    /** Create database representation
+    * @param \PDO
+    * @param \NotORM\Structure\StructureInterface or null for new NotORM_Structure_Convention
+    * @param \NotORM\Cache\CacheInterface or null for no cache
+    */
+    public function __construct(PDO $connection, StructureInterface $structure = null, CacheInterface $cache = null) 
+    {
+        $this->connection = $connection;
+        $this->driver = $connection->getAttribute(\PDO::ATTR_DRIVER_NAME);
+        if (!isset($structure)) {
+            $structure = new Convention;
+        }
+        $this->structure = $structure;
+        $this->cache = $cache;
+    }
+
+    /** Get table data to use as $db->table[1]
+    * @param string
+    * @return \NotORM\Result
+    */
+    public function __get($table) 
+    {
+        return new Result($this->structure->getReferencingTable($table, ''), $this, true);
+    }
+    /**
+     * 
+     * @param \NotORM\Cache\CacheInterface $cache
+     * @return \NotORM\NotORM
+     */
+    public function setCache(CacheInterface $cache)
+    {
+        $this->cache = $cache;
+        return $this;
+    }                
+
+    public function getCache()
+    {
+        return $this->cache;
+    }
+    /** Set write-only properties
+    * @return null
+    */
+    public function __set($name, $value) 
+    {
+        if ($name == "debug" || $name == "freeze" || $name == "rowClass") {
+            $this->$name = $value;
+        }
+        if ($name == "transaction") {
+            switch (strtoupper($value)) {
+                case "BEGIN": return $this->connection->beginTransaction();
+                case "COMMIT": return $this->connection->commit();
+                case "ROLLBACK": return $this->connection->rollback();
+            }
+        }
+    }
+
+    /** Get table data
+    * @param string
+    * @param array (["condition"[, array("value")]]) passed to NotORM_Result::where()
+    * @return \NotORM\Result
+    */
+    public function __call($table, array $where) 
+    {
+        $return = new Result($this->structure->getReferencingTable($table, ''), $this);
+        if ($where) {
+            call_user_func_array(array($return, 'where'), $where);
+        }
+        return $return;
+    }
 	
 }
